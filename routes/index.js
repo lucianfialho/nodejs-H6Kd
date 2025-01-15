b7ac2c8f610905dc2ffcfba3bc3620af784319e7
Lucian Fialho <lucianfialhobp@hotmail.com>
2025-01-14 21:48:21 -0300

    -- add crypto

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const sharp = require('sharp');
 const router = express.Router();
-
+const crypto = require("crypto");
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config()
 
